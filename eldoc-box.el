f8de3466f5ff8d2be1723c3e57ef6e2dbf7704a6
Yuan Fu <casouri@gmail.com>
2018-12-11 16:40:48 -0500

    Change copyright stuff

--- eldoc-box.el
+++ eldoc-box.el
@@ -1,17 +1,30 @@
 ;;; eldoc-box.el --- Display documentation in childframe      -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2018  Yuan Fu
+;; Copyright (C) 2017 Sebastien Chapuis
 
 ;; Version: 1.0
-;; Author: Yuan Fu <casouri@gmail.com>
-;; Not really because I copied code from lsp-ui-doc.el
-;; which is written by Sebastien Chapuis <sebastien@chapu.is>
-;; And I removed the license so the previous commits with license
-;; are not really with license.
-;; I hope I don't get in troubles because of this.
+
+;; Author: Sebastien Chapuis <sebastien@chapu.is>
+;; Yuan Fu <casouri@gmail.com> made a lot of change to use it for ElDoc
 ;; URL: https://github.com/casouri/eldoc-box
 ;; Package-Requires: ((emacs "26.1"))
 
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
 ;;; This file is NOT part of GNU Emacs
 
 ;;; Commentary:
