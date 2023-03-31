d1b8cb5066544c60552ad017143a927912f9fc72
Yuan Fu <casouri@gmail.com>
2023-03-31 11:58:20 -0700

    * eldoc-box.el (eldoc-box--display): Workaround (issue#66).

--- eldoc-box.el
+++ eldoc-box.el
@@ -280,6 +280,9 @@ STR has to be a proper documentation, not empty string, not nil, etc."
   (let ((doc-buffer (get-buffer-create eldoc-box--buffer)))
     (with-current-buffer doc-buffer
       (setq mode-line-format nil)
+      ;; WORKAROUND: (issue#66) If cursor-type is ‘box’, sometimes the
+      ;; cursor is still shown for some reason.
+      (setq-local cursor-type t)
       (when (bound-and-true-p global-tab-line-mode)
         (setq tab-line-format nil))
       ;; without this, clicking childframe will make doc buffer the current buffer
